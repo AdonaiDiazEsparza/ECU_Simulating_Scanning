a1ccbc1170ae2d791e24f9ec40075e155c77eb2c
Donuts Diaz <carlosadonai.de0208@gmail.com>
2024-08-04 00:02:36 -0600

    feat: code changed to read only the RPM

--- SEND_CAN/SEND_CAN.ino
+++ SEND_CAN/SEND_CAN.ino
@@ -9,12 +9,14 @@
 
 unsigned long txId = 0x7DF;
 unsigned char txlen = 8;
-unsigned char txBuf[8] = {0x02, 0x01, 0x0D, 0x00, 0x00, 0x00, 0x00, 0x00};  // For the moment it will request for RPM if you want to ask for other PID code chage the 
+unsigned char txBuf[8] = {0x02, 0x01, 0x0C, 0x00, 0x00, 0x00, 0x00, 0x00};  // For the moment it will request for RPM if you want to ask for other PID code chage the 
                                                                             // 1rst byte for the quantity of bytes, 2nd the mode, 3rd the PID code.
 
 long unsigned int rxId;
 unsigned char len = 0;
-unsigned char rxBuf[8];     
+unsigned char rxBuf[8];  
+
+unsigned long tiempo = 0;
 
 #define CAN0_INT 2                              // Set INT to pin 2
 MCP_CAN CAN0(10);                               // Set CS to pin 10
@@ -29,32 +31,39 @@ void setup() {
   }
   Serial.println("MCP2515 Initialize");
   CAN0.setMode(MCP_NORMAL);  
+
+  tiempo = millis();
 }
 
 void loop() {
       if(!digitalRead(3)){
-        if(CAN0.sendMsgBuf(txId,txlen, txBuf) == CAN_OK){
+        CAN0.sendMsgBuf(txId,txlen, txBuf);
+        /*if(CAN0.sendMsgBuf(txId,txlen, txBuf) == CAN_OK){
           Serial.println("Message sent okay");
         }
         else{
           Serial.println("Message sent wrong");
-        }
-        delay(100);
+        }*/
+        delay(10);
       }
    
 
   if(!digitalRead(2)){
     CAN0.readMsgBuf(&rxId,&len,rxBuf);
     if(rxId == 0x7E8){
-      Serial.print("Id: ");
-      Serial.print(rxId,HEX);
 
-      for(int i = 0; i<len; i++){
-        Serial.print("  ");
-        Serial.print(rxBuf[i],HEX);
-      }
-      Serial.println();
-      delay(10);
+      long rpmA = 64*rxBuf[3];
+      long rpmB = rxBuf[4]/4;
+
+      long rpm = rpmA+rpmB;
+
+      long muestreo = millis()-tiempo;
+
+      Serial.println("RPM SPEED: "+String(rpm)+"rpm  | "+String(muestreo)+" ms");
+
+      tiempo = millis();
+      
+      delay(5);
     }
    }
 }
